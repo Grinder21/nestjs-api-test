acf64466f060aa90343b82556722120dce281e6b
Grinder21 <spartawinner280@mail.ru>
2023-10-17 23:31:03 +0300

    add: use jest for getposts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './app.controller';
 import { AppService } from './app.service';
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
 
 describe('PostsController', () => {
   let appController: PostsController;
@@ -9,16 +11,40 @@ describe('PostsController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
-      providers: [AppService, HttpService],
+      providers: [AppService],
       imports: [HttpModule],
     }).compile();
 
     appController = app.get<PostsController>(PostsController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  it('should get posts', (done) => {
+    const posts = [{ id: 1, title: 'test post' }];
+    const mockedAxiosResponse: AxiosResponse = {
+      data: posts,
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {
+        headers: undefined,
+      },
+    };
+
+    jest
+      .spyOn(appController, 'getPosts')
+      .mockImplementation(() => of(mockedAxiosResponse));
+
+    appController.getPosts().subscribe({
+      next: (val) => {
+        expect(val).toBe(posts);
+        done();
+      },
+      error: (err) => {
+        done(err);
+      },
+      complete: () => {
+        done();
+      },
     });
   });
 });
